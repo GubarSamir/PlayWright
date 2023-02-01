8c4998f5bd985aa49c9f44bd03fa87f51fe225d9
OFFICEKIEV\sa.gubar <sa.gubar@temabit.com>
2023-02-01 16:30:00 +0200

    comment screen

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -22,15 +22,15 @@ test('get started link', async ({ page }) => {
 });
 
 
-test('scrrenshot', async () => {
-    for (const browserType of [webkit, chromium, firefox]){
-        const browser = await browserType.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.fozzy.ua/ua/restaurants/');
-        await page.screenshot({
-            path: `screenshot-${browserType.name()}.png`,
-        });
-        await browser.close();
-        console.log('succes ' + browserType.name())
-    }
-});
+// test('scrrenshot', async () => {
+//     for (const browserType of [webkit, chromium, firefox]){
+//         const browser = await browserType.launch();
+//         const page = await browser.newPage();
+//         await page.goto('https://www.fozzy.ua/ua/restaurants/');
+//         await page.screenshot({
+//             path: `screenshot-${browserType.name()}.png`,
+//         });
+//         await browser.close();
+//         console.log('succes ' + browserType.name())
+//     }
+// });
