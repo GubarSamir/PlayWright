8fa94386c8e3c8a90cf0b923b1be7352b62fdabb
OFFICEKIEV\sa.gubar <sa.gubar@temabit.com>
2023-02-01 14:26:20 +0200

    new test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,33 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
+const {webkit, chromium, firefox} = require('playwright');
+
+(async () => {
+    for (const browserType of [webkit, chromium, firefox]){
+        const browser = await browserType.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.fozzy.ua/ua/restaurants/');
+        await page.screenshot({
+            path: `screenshot-${browserType.name()}.png`,
+        });
+        await browser.close();
+        console.log('succes ' + browserType.name())
+    }
+})();
+
