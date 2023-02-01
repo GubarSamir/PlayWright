e315b10250218819350e553624995f5f5adfd601
OFFICEKIEV\sa.gubar <sa.gubar@temabit.com>
2023-02-01 12:50:56 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+const {webkit, chromium, firefox} = require('playwright');
+
+(async () => {
+    for (const browserType of [webkit, chromium, firefox]){
+        const browser = await browserType.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.fozzy.ua/ua/restaurants/');
+        await page.screenshot({
+            path: `screenshot-${browserType.name()}.png`,
+        });
+        await browser.close();
+        console.log('succes ' + browserType.name())
+    }
+})();
+
