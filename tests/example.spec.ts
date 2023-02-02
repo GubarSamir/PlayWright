32acbf67cefff4042478652be15580a7bc24bb16
OFFICEKIEV\sa.gubar <sa.gubar@temabit.com>
2023-02-02 16:14:02 +0200

    return

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect, type Page } from '@playwright/test';
-const {webkit, chromium, firefox, devices, request} = require('playwright');
-const iPhone13Pro = devices['iPhone 11 Pro'];
-
+const {webkit, chromium, firefox} = require('playwright');
 
 test('screenshot', async () => {
     for (const browserType of [webkit, chromium, firefox]){
@@ -15,48 +13,3 @@ test('screenshot', async () => {
         console.log('succes ' + browserType.name())
     }
 });
-
-
-test('openPnone', async () => {
-    const browser = await webkit.launch({
-        headless: false,
-    });
-    const context = await browser.newContext({
-        ...iPhone13Pro, 
-    });
-    const page = await context.newPage();
-    await page.goto('https://www.fozzy.ua/ua/restaurants/');
-});
-
-
-test('openGeolocation', async () => {
-    const browser = await chromium.launch({
-        headless: false,
-    });
-    const context = await browser.newContext({
-        geolocation:{latitude: 51.508076, longitude: -0.0993827,},
-        permissions: ['geolocation'],
-    });
-    const page = await context.newPage();
-    await page.goto('https:/maps.google.com');
-});
-
-
-test('stopAdvertising', async () => {
-        const browser = await chromium.launch({
-            headless: false,
-        });
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        page.on('frameattached', frame => console.log('frames '+ page.frames().length));
-        page.on('frameattached', frame => console.log('frames '+ page.frames().length));
-        page.on('request', request => console.log(request.method() + ' ' + request.url()));
-        page.on('response', response => console.log(response.status() + ' ' + response.url()));
-        context.route('**/*', route => {
-            if (route.request.frame().parentFrame())
-                route.abort();
-            else
-                route.continue;
-        })
-        await page.goto('https://www.fozzy.ua/ua/restaurants/');
-    });
